655e8941925618704c59d91345cc14afd3e8263e
Anders Emil <ae@cinemataztic.com>
2022-11-17 11:35:43 +0100

    AssemblyInfos added. Author changed to "Cinemataztic"

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("CineGame.MobileEditor")]
+[assembly: AssemblyDescription ("UnityEditor scripts for editing, building and uploading mobile assets for CineGame")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Cinemataztic")]
+[assembly: AssemblyProduct ("CineGameSDK")]
+[assembly: AssemblyCopyright ("2021 CinemaTaztic Aps")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.0.0")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
