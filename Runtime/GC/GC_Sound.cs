8bf3a727c63e53853cfb1ac40074758a465c1741
Anders Emil <ae@cinemataztic.com>
2022-11-22 14:34:27 +0100

    GC_Sound: Guard against null references when game does not have GUI sound controls

--- Runtime/GC/GC_Sound.cs
+++ Runtime/GC/GC_Sound.cs
@@ -7,216 +7,193 @@ public class GC_Sound : MonoBehaviour {
 
     public static GC_Sound instance;
 
-    [Header("Audio")]
+    [Header ("Audio")]
     public Button AudioButton;
     public GameObject AudioContainer;
 
     private bool showAudioContainer = false;
 
-    [Header("Music")]
+    [Header ("Music")]
     public Button MusicMuteButton;
     public Slider MusicVolumeSlider;
     public Sprite MusicMuteSprite;
-	public Sprite MusicUnMuteSprite;
+    public Sprite MusicUnMuteSprite;
 
-	private AudioSource musicAudioSource;
+    private AudioSource musicAudioSource;
 
-    [Header("SFX")]
+    [Header ("SFX")]
     public Button SFXMuteButton;
     public Slider SFXVolumeSlider;
     public Sprite SFXMuteSprite;
     public Sprite SFXUnMuteSprite;
 
-    private List<AudioSource> sfxAudioSources = new List<AudioSource>();
+    private List<AudioSource> sfxAudioSources = new List<AudioSource> ();
 
-   
 
-	void Awake() {
+
+    void Awake () {
 
         instance = this;
         musicAudioSource = gameObject.GetComponent<AudioSource> ();
-	}
+    }
 
-	void Start () {
+    void Start () {
 
         if (musicAudioSource != null) {
             musicAudioSource.loop = true;
             musicAudioSource.Play ();
         }
 
-        if (PlayerPrefs.HasKey("GameCenterMusic"))
-        {
-            if (PlayerPrefs.GetString("GameCenterMusic") == "On")
-            {
-                MusicMuteButton.GetComponent<Image>().sprite = MusicUnMuteSprite;
+        if (PlayerPrefs.HasKey ("GameCenterMusic")) {
+            if (PlayerPrefs.GetString ("GameCenterMusic") == "On") {
+                if (MusicMuteButton != null) {
+                    MusicMuteButton.GetComponent<Image> ().sprite = MusicUnMuteSprite;
+                }
                 musicAudioSource.mute = false;
-            }
-            else
-            {
-                MusicMuteButton.GetComponent<Image>().sprite = MusicMuteSprite;
+            } else {
+                if (MusicMuteButton != null) {
+                    MusicMuteButton.GetComponent<Image> ().sprite = MusicMuteSprite;
+                }
                 musicAudioSource.mute = true;
             }
-        }
-        else
-        {
-            PlayerPrefs.SetString("GameCenterMusic", "On");
+        } else {
+            PlayerPrefs.SetString ("GameCenterMusic", "On");
         }
 
-        if (PlayerPrefs.HasKey("GameCenterMusicVolume"))
-        {
-            musicAudioSource.volume = PlayerPrefs.GetFloat("GameCenterMusicVolume");
-            MusicVolumeSlider.value = PlayerPrefs.GetFloat("GameCenterMusicVolume");
-        }
-        else
-        {
-            PlayerPrefs.SetFloat("GameCenterMusicVolume", 1);
-        }
-
-        if (PlayerPrefs.HasKey("GameCenterSFX"))
-        {
-            if (PlayerPrefs.GetString("GameCenterSFX") == "On")
-            {
-                SFXMuteButton.GetComponent<Image>().sprite = SFXUnMuteSprite;
+        if (PlayerPrefs.HasKey ("GameCenterMusicVolume")) {
+            musicAudioSource.volume = PlayerPrefs.GetFloat ("GameCenterMusicVolume");
+            if (MusicVolumeSlider != null) {
+                MusicVolumeSlider.value = PlayerPrefs.GetFloat ("GameCenterMusicVolume");
             }
-            else
-            {
-                SFXMuteButton.GetComponent<Image>().sprite = SFXMuteSprite;
+        } else {
+            PlayerPrefs.SetFloat ("GameCenterMusicVolume", 1);
+        }
+
+        if (PlayerPrefs.HasKey ("GameCenterSFX")) {
+            if (SFXMuteButton != null) {
+                if (PlayerPrefs.GetString ("GameCenterSFX") == "On") {
+                    SFXMuteButton.GetComponent<Image> ().sprite = SFXUnMuteSprite;
+                } else {
+                    SFXMuteButton.GetComponent<Image> ().sprite = SFXMuteSprite;
+                }
             }
-        }
-        else
-        {
-            PlayerPrefs.SetString("GameCenterSFX", "On");
+        } else {
+            PlayerPrefs.SetString ("GameCenterSFX", "On");
         }
 
-        if (PlayerPrefs.HasKey("GameCenterSFXVolume"))
-        {
-            SFXVolumeSlider.value = PlayerPrefs.GetFloat("GameCenterSFXVolume");
-        }
-        else
-        {
-            PlayerPrefs.SetFloat("GameCenterSFXVolume", 1);
+        if (PlayerPrefs.HasKey ("GameCenterSFXVolume")) {
+            if (SFXVolumeSlider != null) {
+                SFXVolumeSlider.value = PlayerPrefs.GetFloat ("GameCenterSFXVolume");
+            }
+        } else {
+            PlayerPrefs.SetFloat ("GameCenterSFXVolume", 1);
         }
 
-        AudioButton.onClick.AddListener(ToggleAudioContainer);
-        MusicMuteButton.onClick.AddListener(MuteMusic);
-        MusicVolumeSlider.onValueChanged.AddListener(delegate { ChangeMusicVolume(); });
-        SFXMuteButton.onClick.AddListener(MuteSFX);
-        SFXVolumeSlider.onValueChanged.AddListener(delegate { ChangeSFXVolume(); });
-        AudioContainer.SetActive(false);
-
+        if (AudioButton != null) {
+            AudioButton.onClick.AddListener (ToggleAudioContainer);
+        }
+        if (MusicMuteButton != null) {
+            MusicMuteButton.onClick.AddListener (MuteMusic);
+        }
+        if (MusicVolumeSlider != null) {
+            MusicVolumeSlider.onValueChanged.AddListener (delegate { ChangeMusicVolume (); });
+        }
+        if (SFXMuteButton != null) {
+            SFXMuteButton.onClick.AddListener (MuteSFX);
+        }
+        if (SFXVolumeSlider != null) {
+            SFXVolumeSlider.onValueChanged.AddListener (delegate { ChangeSFXVolume (); });
+        }
+        if (AudioContainer != null) {
+            AudioContainer.SetActive (false);
+        }
     }
 
-    public static void PlaySound(AudioClip soundClip)
-    {
-        if (PlayerPrefs.GetString("GameCenterSFX") == "Off")
-        {
+    public static void PlaySound (AudioClip soundClip) {
+        if (PlayerPrefs.GetString ("GameCenterSFX") == "Off") {
             return;
         }
 
         AudioSource targetAudioSource = null;
 
-        foreach (var item in instance.sfxAudioSources)
-        {
-            if(!item.isPlaying)
-            {
+        foreach (var item in instance.sfxAudioSources) {
+            if (!item.isPlaying) {
                 targetAudioSource = item;
             }
         }
 
-        if(targetAudioSource == null)
-        {
-            GameObject newObject = new GameObject("Sound");
-            newObject.transform.SetParent(instance.transform);
+        if (targetAudioSource == null) {
+            GameObject newObject = new GameObject ("Sound");
+            newObject.transform.SetParent (instance.transform);
             newObject.transform.localPosition = Vector3.zero;
-            targetAudioSource = newObject.AddComponent<AudioSource>();
-            instance.sfxAudioSources.Add(targetAudioSource);
+            targetAudioSource = newObject.AddComponent<AudioSource> ();
+            instance.sfxAudioSources.Add (targetAudioSource);
         }
 
 
         targetAudioSource.clip = soundClip;
-        targetAudioSource.volume = PlayerPrefs.GetFloat("GameCenterSFXVolume");
-        targetAudioSource.Play();
-    }    
+        targetAudioSource.volume = PlayerPrefs.GetFloat ("GameCenterSFXVolume");
+        targetAudioSource.Play ();
+    }
 
-    public void ToggleAudioContainer()
-    {
-        if (!showAudioContainer)
-        {
-            AudioContainer.SetActive(true);
+    public void ToggleAudioContainer () {
+        if (!showAudioContainer) {
+            AudioContainer.SetActive (true);
             showAudioContainer = true;
-        }
-        else
-        {
-            AudioContainer.SetActive(false);
+        } else {
+            AudioContainer.SetActive (false);
             showAudioContainer = false;
         }
 
     }
 
-    private void MuteMusic()
-    {
-        if (PlayerPrefs.GetString("GameCenterMusic") == "On")
-        {
-            PlayerPrefs.SetString("GameCenterMusic", "Off");
-            MusicMuteButton.GetComponent<Image>().sprite = MusicMuteSprite;
+    private void MuteMusic () {
+        if (PlayerPrefs.GetString ("GameCenterMusic") == "On") {
+            PlayerPrefs.SetString ("GameCenterMusic", "Off");
+            MusicMuteButton.GetComponent<Image> ().sprite = MusicMuteSprite;
             musicAudioSource.mute = true;
-        }
-        else
-        {
-            PlayerPrefs.SetString("GameCenterMusic", "On");
-            MusicMuteButton.GetComponent<Image>().sprite = MusicUnMuteSprite;
+        } else {
+            PlayerPrefs.SetString ("GameCenterMusic", "On");
+            MusicMuteButton.GetComponent<Image> ().sprite = MusicUnMuteSprite;
             musicAudioSource.mute = false;
         }
     }
 
-    void ChangeMusicVolume()
-    {
-        PlayerPrefs.SetFloat("GameCenterMusicVolume", MusicVolumeSlider.value);
+    void ChangeMusicVolume () {
+        PlayerPrefs.SetFloat ("GameCenterMusicVolume", MusicVolumeSlider.value);
 
-        if (MusicVolumeSlider.value == 0)
-        {
-            PlayerPrefs.SetString("GameCenterMusic", "Off");
-            MusicMuteButton.GetComponent<Image>().sprite = MusicMuteSprite;
+        if (MusicVolumeSlider.value == 0) {
+            PlayerPrefs.SetString ("GameCenterMusic", "Off");
+            MusicMuteButton.GetComponent<Image> ().sprite = MusicMuteSprite;
             musicAudioSource.mute = true;
-        }
-        else
-        {
-            PlayerPrefs.SetString("GameCenterMusic", "On");
-            MusicMuteButton.GetComponent<Image>().sprite = MusicUnMuteSprite;
+        } else {
+            PlayerPrefs.SetString ("GameCenterMusic", "On");
+            MusicMuteButton.GetComponent<Image> ().sprite = MusicUnMuteSprite;
             musicAudioSource.mute = false;
         }
 
         musicAudioSource.volume = MusicVolumeSlider.value;
     }
 
-    private void MuteSFX()
-    {
-        if (PlayerPrefs.GetString("GameCenterSFX") == "On")
-        {
-            PlayerPrefs.SetString("GameCenterSFX", "Off");
-            SFXMuteButton.GetComponent<Image>().sprite = SFXMuteSprite;
-        }
-        else
-        {
-            PlayerPrefs.SetString("GameCenterSFX", "On");
-            SFXMuteButton.GetComponent<Image>().sprite = SFXUnMuteSprite;
+    private void MuteSFX () {
+        if (PlayerPrefs.GetString ("GameCenterSFX") == "On") {
+            PlayerPrefs.SetString ("GameCenterSFX", "Off");
+            SFXMuteButton.GetComponent<Image> ().sprite = SFXMuteSprite;
+        } else {
+            PlayerPrefs.SetString ("GameCenterSFX", "On");
+            SFXMuteButton.GetComponent<Image> ().sprite = SFXUnMuteSprite;
         }
     }
 
-    void ChangeSFXVolume()
-    {
-        PlayerPrefs.SetFloat("GameCenterSFXVolume", SFXVolumeSlider.value);
+    void ChangeSFXVolume () {
+        PlayerPrefs.SetFloat ("GameCenterSFXVolume", SFXVolumeSlider.value);
 
-        if (SFXVolumeSlider.value == 0)
-        {
-            PlayerPrefs.SetString("GameCenterSFX", "Off");
-            SFXMuteButton.GetComponent<Image>().sprite = SFXMuteSprite;
-        }
-        else
-        {
-            PlayerPrefs.SetString("GameCenterSFX", "On");
-            SFXMuteButton.GetComponent<Image>().sprite = SFXUnMuteSprite;
+        if (SFXVolumeSlider.value == 0) {
+            PlayerPrefs.SetString ("GameCenterSFX", "Off");
+            SFXMuteButton.GetComponent<Image> ().sprite = SFXMuteSprite;
+        } else {
+            PlayerPrefs.SetString ("GameCenterSFX", "On");
+            SFXMuteButton.GetComponent<Image> ().sprite = SFXUnMuteSprite;
         }
     }
 }
-
