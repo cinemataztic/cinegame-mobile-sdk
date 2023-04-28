9f703f8748e41fb9c5b8f83d95b1835c90c53198
Anders Emil <ae@cinemataztic.com>
2023-04-28 11:59:25 +0200

    Added RaycastComponent to support point-and-click etc

---
+++ Editor/RaycastEditor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+using CineGame.MobileComponents;
+
+namespace CineGameEditor.MobileComponents {
+	[CustomEditor (typeof (RaycastComponent))]
+	[CanEditMultipleObjects]
+	public class RaycastEditor : EventEditorBase {
+	}
+}
