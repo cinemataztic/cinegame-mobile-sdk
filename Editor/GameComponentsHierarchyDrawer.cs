a335695e18be75a185a5d3240926619f9ec79fdf
Anders Emil <ae@cinemataztic.com>
2023-05-25 11:48:08 +0200

    Reenabled app icon in GameComponentsHierarchyDrawer with better layout and mipmaps

--- Editor/GameComponentsHierarchyDrawer.cs
+++ Editor/GameComponentsHierarchyDrawer.cs
@@ -11,7 +11,7 @@ namespace CineGameEditor.MobileComponents {
 		static Texture2D texturePanel;
 		static Texture2D textureAppIcon;
 
-		static List<IGameComponentIcon> gameComponentsList = new List<IGameComponentIcon> ();
+		static List<IGameComponentIcon> gameComponentsList = new ();
 
 		static GameComponentsHierarchyDrawer () {
 			// Init
@@ -37,18 +37,24 @@ namespace CineGameEditor.MobileComponents {
 		/// We prefer android icons as they contain alpha, but fall back on default if Android is not overridden.
 		/// </summary>
 		public static void ReloadAppIcon () {
-			var textures = PlayerSettings.GetIconsForTargetGroup (BuildTargetGroup.Android);
+			var textures = PlayerSettings.GetIcons (UnityEditor.Build.NamedBuildTarget.Android, IconKind.Application);
 			if (textures == null || textures.Length == 0 || textures [0] == null) {
-				textures = PlayerSettings.GetIconsForTargetGroup (BuildTargetGroup.Unknown);
+				textures = PlayerSettings.GetIcons (UnityEditor.Build.NamedBuildTarget.Unknown, IconKind.Application);
 			}
 			if (textures != null && textures.Length > 0 && textures [0] != null) {
-				textureAppIcon = textures [0];
+				var t = textures [0];
+				textureAppIcon = new Texture2D (t.width, t.height, t.format, true);
+				Graphics.CopyTexture (t, 0, 0, textureAppIcon, 0, 0);
+				textureAppIcon.Apply (true, true);
+				AppIconContent = new GUIContent (textureAppIcon) {
+					tooltip = $"{PlayerSettings.productName} {Util.GetRegion ()}"
+				};
 			}
 			EditorApplication.RepaintHierarchyWindow ();
 		}
 
+		static GUIContent AppIconContent;
 		static Transform prevItemTransform;
-		static bool prevItemHadGameComponentInChildren;
 		static Rect prevItemIconRect;
 
 		static void HierarchyItemCB (int instanceID, Rect selectionRect) {
@@ -69,26 +75,23 @@ namespace CineGameEditor.MobileComponents {
 				// Item is a gameobject. if it has a GameComponent directly attached, draw icon with full opacity
 				var xpos = selectionRect.x + selectionRect.width - 16;
 				var r = new Rect (xpos, selectionRect.y, 18, 18);
-				go.GetComponents<IGameComponentIcon> (gameComponentsList);
+				go.GetComponents (gameComponentsList);
 				if (gameComponentsList.Count != 0) {
-					go.GetComponentsInChildren<IGameComponentIcon> (true, gameComponentsList);
+					go.GetComponentsInChildren (true, gameComponentsList);
 					GUI.Label (r, new GUIContent (texturePanel, string.Join ('\n', gameComponentsList.Select (gc => gc.GetType ().Name).Distinct ())));
 					gameComponentsList.Clear ();
 				} else {
-					go.GetComponentsInChildren<IGameComponentIcon> (true, gameComponentsList);
+					go.GetComponentsInChildren (true, gameComponentsList);
 					prevItemIconRect = r;
 					prevItemTransform = go.transform;
 				}
 			} else {
-				/*// Item is a scene. Draw app icon and region
-				size += 7f;
-				Rect r;
-				if (textureAppIcon != null) {
-					r = new Rect (selectionRect.x + selectionRect.width - size, selectionRect.y - 2.5f, size, size);
-					GUI.Label (r, textureAppIcon);
+				// Item is a scene. Draw app icon and region, if available
+				if (AppIconContent != null) {
+					var size = selectionRect.height + 6f;
+					var r = new Rect (selectionRect.x - 3f, selectionRect.y - 2f, size, size);
+					GUI.Label (r, AppIconContent);
 				}
-				r = new Rect (selectionRect.x + selectionRect.width - size - 24, selectionRect.y, 24, size - 8f);
-				GUI.Label (r, Util.GetRegion ().ToString ());*/
 			}
 		}
 	}
