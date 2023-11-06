2352273b6231ae7a57f5e8810d0dc2ffa35b0296
Anders Emil <ae@cinemataztic.com>
2023-11-06 11:55:10 +0100

    Added BroadcastMessage runtime Component for broadcasting messages to gameobjects without requiring a receiver (so not logging an error if there are child gameobjects without a receiver)

---
+++ Runtime/BroadcastMessage.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace CineGame.MobileComponents {
+
+	[ComponentReference ("Utility to invoke GameObject.BroadcastMessage with the \"DontRequireReceiver\" option, ie without logging an error if children do not have a receiving method.\n\nYou can also supply a dynamic Object or a string as parameter.")]
+	public class BroadcastMessage : BaseComponent {
+
+		[Tooltip ("Predefined Message. You can override this by invoking the Broadcast (string) method")]
+		public string Message;
+
+		/// <summary>
+		/// Broadcast a dynamic message, ignoring the predefined Message
+		/// </summary>
+		public void Broadcast (string Message) {
+			BroadcastMessage (Message, SendMessageOptions.DontRequireReceiver);
+		}
+
+		/// <summary>
+		/// Broadcast the predefined Message with a dynamic parameter
+		/// </summary>
+		public void Broadcast (Object parameter) {
+			BroadcastMessage (Message, parameter, SendMessageOptions.DontRequireReceiver);
+		}
+
+		/// <summary>
+		/// Broadcast the predefined Message with a dynamic string
+		/// </summary>
+		public void BroadcastString (string parameter) {
+			BroadcastMessage (Message, parameter, SendMessageOptions.DontRequireReceiver);
+		}
+
+	}
+}
