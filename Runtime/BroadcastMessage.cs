dfea3deaaeca5c9f1fc9d14b9b089a4345bd0e8a
Anders Emil <ae@cinemataztic.com>
2024-09-16 16:09:43 +0200

    BroadcastMessage can now send the message upwards too (GameObject.SendMessageUpwards)

--- Runtime/BroadcastMessage.cs
+++ Runtime/BroadcastMessage.cs
@@ -2,7 +2,7 @@
 
 namespace CineGame.MobileComponents {
 
-	[ComponentReference ("Utility to invoke GameObject.BroadcastMessage with the \"DontRequireReceiver\" option, ie without logging an error if children do not have a receiving method.\n\nYou can also supply a dynamic Object or a string as parameter.")]
+	[ComponentReference ("Utility to invoke GameObject.BroadcastMessage or GameObject.SendUpwards with the \"DontRequireReceiver\" option, ie without logging an error if children or parents do not have a receiving method.\n\nYou can also supply a dynamic Object or a string as parameter.")]
 	public class BroadcastMessage : BaseComponent {
 
 		[Tooltip ("Predefined Message. You can override this by invoking the Broadcast (string) method")]
@@ -11,8 +11,8 @@ namespace CineGame.MobileComponents {
 		/// <summary>
 		/// Broadcast a dynamic message, ignoring the predefined Message
 		/// </summary>
-		public void Broadcast (string Message) {
-			BroadcastMessage (Message, SendMessageOptions.DontRequireReceiver);
+		public void Broadcast (string message) {
+			BroadcastMessage (message, SendMessageOptions.DontRequireReceiver);
 		}
 
 		/// <summary>
@@ -29,5 +29,25 @@ namespace CineGame.MobileComponents {
 			BroadcastMessage (Message, parameter, SendMessageOptions.DontRequireReceiver);
 		}
 
+		/// <summary>
+        /// Send a dynamic message upwards, ignoring the predefined Message
+        /// </summary>
+		public void SendUpwards (string message) {
+			SendMessageUpwards (message, SendMessageOptions.DontRequireReceiver);
+		}
+
+		/// <summary>
+		/// Send the predefined message upwards with a dynamic parameter
+		/// </summary>
+		public void SendUpwards (Object parameter) {
+			SendMessageUpwards (Message, parameter, SendMessageOptions.DontRequireReceiver);
+		}
+
+		/// <summary>
+        /// Send the predefined message upwards with a dynamic string
+        /// </summary>
+		public void SendStringUpwards (string parameter) {
+			SendMessageUpwards (Message, parameter, SendMessageOptions.DontRequireReceiver);
+		}
 	}
 }
