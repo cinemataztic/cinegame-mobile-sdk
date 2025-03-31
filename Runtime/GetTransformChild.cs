9f36d6543bc503da60b4d367284d14f1deaab4ea
Anders Emil <ae@cinemataztic.com>
2025-03-31 12:00:49 +0200

    GetTransformChild: Added modes 'BackSequential' and 'RandomSequential'

--- Runtime/GetTransformChild.cs
+++ Runtime/GetTransformChild.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace CineGame.MobileComponents {
 
 	[ComponentReference ("Pass through one of a transform's children. Each interval or each time UpdateNow is called, a new child is selected. 'Mode' determines in which order.")]
 	public class GetTransformChild : BaseComponent {
 
+		[Tooltip ("The parent of the children. If none specified, this gameobject is used as Source")]
 		public Transform Source;
 		[Tooltip ("Update interval in msecs. 0=One-shot")]
 		public float UpdateInterval = 0f;
@@ -13,44 +15,142 @@ namespace CineGame.MobileComponents {
 		public enum SequenceMode {
 			Random,
 			Sequential,
+			BackSequential,
+			RandomSequential,
 		}
 		public SequenceMode Mode = SequenceMode.Random;
 
 		[Tooltip("Invoked with the Source's selected child")]
-		public UnityEvent<Transform> OnUpdate;
+		[FormerlySerializedAs ("OnUpdate")]
+		public UnityEvent<Transform> OnSelect;
 
 		float lastSetTime = float.MinValue;
 		int lastIndex = -1;
+		bool backSequentialUpdate;
 
-		private void OnEnable () {
+		int [] RandomSequentialIndices;
+		int RandomSequentialCounter;
+
+		void OnEnable () {
 			if (Source == null && UpdateInterval > float.Epsilon) {
 				Source = transform;
 			}
+			if (Mode == SequenceMode.RandomSequential) {
+				InitRandomSequentialIndices ();
+			}
+			lastIndex = -1;
+			backSequentialUpdate = false;
 			if (Source != null) {
 				UpdateNow ();
 			}
 		}
 
+		/// <summary>
+        /// Set a new Source transform to get children from
+        /// </summary>
 		public void SetSource (Transform s) {
 			Source = s;
 			OnEnable ();
 		}
 
+		/// <summary>
+        /// Choose a new child now
+        /// </summary>
 		public void UpdateNow () {
 			lastSetTime = Time.time;
-			var index = (lastIndex + ((Mode == SequenceMode.Random) ? Random.Range (1, Source.childCount) : 1)) % Source.childCount;
-			var child = Source.GetChild (index);
-			Log ($"GetTransformChild.UpdateNow {child.gameObject.GetScenePath ()}");
-			lastIndex = index;
-			OnUpdate?.Invoke (child);
+			var newIndex = 0;
+			switch (Mode) {
+			case SequenceMode.Sequential:
+				newIndex = (lastIndex + 1) % Source.childCount;
+				break;
+			case SequenceMode.BackSequential:
+				newIndex = lastIndex;
+				if (backSequentialUpdate) newIndex--;
+				backSequentialUpdate = true;
+				if (newIndex < 0)
+					newIndex += Source.childCount;
+				break;
+			case SequenceMode.RandomSequential:
+				newIndex = RandomSequentialIndices [RandomSequentialCounter++];
+				if (RandomSequentialCounter >= RandomSequentialIndices.Length)
+					RandomSequentialCounter = 0;
+				break;
+			case SequenceMode.Random:
+				newIndex = Random.Range (0, Source.childCount);
+				break;
+			}
+			var child = Source.GetChild (newIndex);
+			Log ($"GetTransformChild.UpdateNow lastIndex={lastIndex} newIndex={newIndex} name={child.name}");
+			lastIndex = newIndex;
+			OnSelect?.Invoke (child);
 		}
 
-		private void Update () {
+		/// <summary>
+		/// Activate the specified transform and deactivate all its siblings
+		/// </summary>
+		public void ActivateSingle (Transform t) {
+			var parent = t.parent;
+			var index = t.GetSiblingIndex ();
+			for (int i = 0; i < parent.childCount; i++) {
+				parent.GetChild (i).gameObject.SetActive (i == index);
+			}
+		}
+
+		/// <summary>
+        /// Activate the transform's gameobject
+		/// </summary>
+		public void Activate (Transform t) {
+			t.gameObject.SetActive (true);
+		}
+
+		/// <summary>
+		/// Deactivate the transform's gameobject
+		/// </summary>
+		public void Deactivate (Transform t) {
+			t.gameObject.SetActive (false);
+		}
+
+		/// <summary>
+		/// All children of Source will be activated or deactivated
+		/// </summary>
+		public void SetActiveChildren (bool active) {
+			for (int i = 0; i < Source.childCount; i++) {
+				Source.GetChild (i).gameObject.SetActive (active);
+			}
+		}
+
+		void Update () {
 			if (Source != null) {
 				if (UpdateInterval > float.Epsilon && (lastSetTime + UpdateInterval) <= Time.time) {
 					UpdateNow ();
 				}
 			}
 		}
-	}
+
+		/// <summary>
+        /// Init array of child indices for the random sequential mode
+        /// </summary>
+		void InitRandomSequentialIndices () {
+			var num = Source.childCount;
+			RandomSequentialIndices = new int [num];
+			RandomSequentialCounter = 0;
+			for (int i = 0; i < num; i++) {
+				RandomSequentialIndices [i] = i;
+			}
+			for (int i = 0; i < num; i++) {
+				var randomIndex = Random.Range (0, num);
+				if (i == randomIndex)
+					continue;
+				var idx = RandomSequentialIndices [randomIndex];
+				RandomSequentialIndices [randomIndex] = RandomSequentialIndices [i];
+				RandomSequentialIndices [i] = idx;
+			}
+		}
+
+		void OnTransformChildrenChanged () {
+			if (Source == transform && Mode == SequenceMode.RandomSequential) {
+				InitRandomSequentialIndices ();
+			}
+        }
+    }
 }
