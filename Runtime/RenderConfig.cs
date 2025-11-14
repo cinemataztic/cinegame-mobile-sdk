479c8b1b838c5f2cad59f1f4a29967e66a48873d
Anders Emil <ae@cinemataztic.com>
2025-11-14 13:45:59 +0100

    Add properties Ambient Intensity, Ambient Probe, Shadow Quality and all the Fog parameters to RenderConfig

--- Runtime/RenderConfig.cs
+++ Runtime/RenderConfig.cs
@@ -17,10 +17,15 @@ namespace CineGame.MobileComponents {
 
 		public ShadowResolution ShadowResolution = ShadowResolution.High;
 
+		[Tooltip("Shadow render quality: None, Hard Only, or Hard+Soft")]
+		public ShadowQuality ShadowQuality = ShadowQuality.All;
+
 		[Tooltip("Shadow distance from camera. Smaller distance = finer shadows")]
 		public float ShadowDistance = 50;
 		
 		public AmbientMode AmbientMode = AmbientMode.Flat;
+		public float AmbientIntensity;
+		public SphericalHarmonicsL2 AmbientProbe;
 		[Tooltip ("Flat ambient color, or sky color if mode is TriLight")]
 		public Color AmbientSkyColor;
 		[Tooltip ("Only used in TriLight mode")]
@@ -30,9 +35,17 @@ namespace CineGame.MobileComponents {
 		[Tooltip("If mode is skybox, use this material")]
 		public Material SkyboxMaterial;
 
-		public Cubemap CustomReflection;
+		public Texture CustomReflection;
+
+		public bool Fog;
+		public FogMode FogMode;
+		public Color FogColor;
+		public float FogDensity;
+		public float FogStartDistance;
+		public float FogEndDistance;
 
 		void OnEnable () {
+			QualitySettings.shadows = ShadowQuality;
 			QualitySettings.shadowDistance = ShadowDistance;
 			QualitySettings.shadowResolution = ShadowResolution;
 			QualitySettings.antiAliasing = (int)AntiAliasing;
@@ -43,6 +56,14 @@ namespace CineGame.MobileComponents {
 			RenderSettings.customReflection = CustomReflection;
 #endif
 			RenderSettings.ambientMode = AmbientMode;
+			RenderSettings.fog = Fog;
+			RenderSettings.fogMode = FogMode;
+			RenderSettings.fogColor = FogColor;
+			RenderSettings.fogDensity = FogDensity;
+			RenderSettings.fogStartDistance = FogStartDistance;
+			RenderSettings.fogEndDistance = FogEndDistance;
+			RenderSettings.ambientProbe = AmbientProbe;
+			RenderSettings.ambientIntensity = AmbientIntensity;
 			switch (AmbientMode) {
 			case AmbientMode.Skybox:
 				RenderSettings.skybox = SkyboxMaterial;
@@ -59,15 +80,18 @@ namespace CineGame.MobileComponents {
 		}
 
 		void Reset () {
+			ShadowQuality = QualitySettings.shadows;
 			ShadowDistance = QualitySettings.shadowDistance;
 			ShadowResolution = QualitySettings.shadowResolution;
 
 			AntiAliasing = (AALevel)QualitySettings.antiAliasing;
 
 			AmbientMode = RenderSettings.ambientMode;
+			AmbientIntensity = RenderSettings.ambientIntensity;
 			AmbientSkyColor = (AmbientMode == AmbientMode.Flat) ? RenderSettings.ambientLight : RenderSettings.ambientSkyColor;
 			AmbientGroundColor = RenderSettings.ambientGroundColor;
 			AmbientEquatorColor = RenderSettings.ambientEquatorColor;
+			AmbientProbe = RenderSettings.ambientProbe;
 
 			SkyboxMaterial = RenderSettings.skybox;
 #if UNITY_2022_3_OR_NEWER
@@ -75,6 +99,12 @@ namespace CineGame.MobileComponents {
 #else
 			CustomReflection = RenderSettings.customReflection;
 #endif
+			Fog = RenderSettings.fog;
+			FogMode = RenderSettings.fogMode;
+			FogColor = RenderSettings.fogColor;
+			FogDensity = RenderSettings.fogDensity;
+			FogStartDistance = RenderSettings.fogStartDistance;
+			FogEndDistance = RenderSettings.fogEndDistance;
 		}
 
         void OnValidate() {
